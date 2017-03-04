faf4af485ee3bdcd5783097eaff97b61fae11501
ze25800000 <ze258100000@sina.comm>
2017-03-04 14:36:22 +0800

    压缩bundle

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const webpack = require('webpack');
+var BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 module.exports = {
     entry: './src/js/root.js',
     output: {
@@ -22,11 +24,30 @@ module.exports = {
             }
         ]
     },
-    devtool: 'source-map',
     devServer: {
         port: 3000,
     },
     resolve: {
         extensions: ['.js', '.css', '.json', '.jsx']
-    }
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env.NODE_ENV': '"production"'
+        }),
+        new webpack.optimize.DedupePlugin(),
+        new webpack.optimize.UglifyJsPlugin({
+            mangle: true,
+            compress: {
+                warnings: false, // Suppress uglification warnings
+                pure_getters: true,
+                unsafe: true,
+                unsafe_comps: true,
+                screw_ie8: true
+            },
+            output: {
+                comments: false,
+            },
+            exclude: [/\.min\.js$/gi] // skip pre-minified libs
+        }),
+    ]
 };
